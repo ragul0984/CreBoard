73fbdf7593a34248927bfd3121a5e54bb773dee3
Ragul <ragul652770@gmail.com>
2026-03-21 20:20:14 +0530

    mob2

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { LayoutDashboard, Layers, IndianRupee, CreditCard, BarChart3, Calendar, Users, X } from 'lucide-react';
